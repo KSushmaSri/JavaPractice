2dd73fd73db7873680e35ac8566e467ba7f3cec2
KSushmaSri <sushmasri1403@gmail.com>
2025-09-14 21:29:37 +0530

    to find min,max values in array

--- Arrays/MinMax.java
+++ Arrays/MinMax.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class MinMax {
     public static void main(String[] args) {
-        int num[]={1,5,3,4,2};
-        System.out.println();
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter size: ");
+        int n=sc.nextInt();
+        int num[]=new int[n];
+        System.out.println("Enter "+n+" elements:");
+        for(int j=0;j<n;j++){
+            num[j]=sc.nextInt();
+        }
+        int max=num[0];
+        int min=num[0];
+        for(int i=0;i<num.length;i++){
+            if(num[i]>max){
+                max=num[i];
+            }
+            if(num[i]<min){
+                min=num[i];
+            }
+        }
+        System.out.println("Maximum value:"+max);
+        System.out.println("Minimum value:"+min);
+        sc.close();
     }
 }
