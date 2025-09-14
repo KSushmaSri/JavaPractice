f958e36e59dba8ec2f550a6b713148d38067adb2
KSushmaSri <sushmasri1403@gmail.com>
2025-09-14 22:24:42 +0530

    modified

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -15,7 +15,7 @@ public class SecondLargest {
             a[i]=sc.nextInt();
         }
         int first=Integer.MIN_VALUE;
-        int second=Integer.MAX_VALUE;
+        int second=Integer.MIN_VALUE;
         for(int i=0;i<a.length;i++){
         if(a[i]>first){
             second=first;
