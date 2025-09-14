4c3c6b0b5d25f945d6a560c9518b5757c02dbcac
KSushmaSri <sushmasri1403@gmail.com>
2025-09-14 22:26:27 +0530

    Second largest number in array

---
+++ Arrays/SecondSmallest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SecondSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n=sc.nextInt();
+        if (n < 2) {
+            System.out.println("Array must have at least 2 elements!");
+            return;
+        }
+        int a[]=new int[n];
+        System.out.println("Enter "+n+" elements:");
+        for(int i =0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+        int first=Integer.MAX_VALUE;
+        int second=Integer.MAX_VALUE;
+        for(int i=0;i<a.length;i++){
+        if(a[i]<first){
+            second=first;
+            first=a[i];
+        }
+        else if(a[i]<second && a[i]!=first){
+            second=a[i];
+        }
+        }
+        if(second==Integer.MAX_VALUE){
+            System.out.println("No second smallest (may be all are same values)");
+        }
+        else{
+        System.out.println("Second Smallest Number is:"+second);
+        }
+        }
+    }
+
