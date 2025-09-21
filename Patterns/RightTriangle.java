6684b6c456f9667e0fc9a09e968736b326a9fd37
KSushmaSri <sushmasri1403@gmail.com>
2025-09-21 18:52:48 +0530

    added logic for inverted triagle

--- Patterns/RightTriangle.java
+++ Patterns/RightTriangle.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class RightTriangle {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter n : ");
+        System.out.print("Enter n : ");
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // INVERTED TRIANGLE
+        System.out.println("Inverted Triangle: ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
                 System.out.print("*");
             }
             System.out.println();
