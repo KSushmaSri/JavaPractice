3289f14e95a26e4bef6bdf7c254469dd6d95ab7c
KSushmaSri <sushmasri1403@gmail.com>
2025-11-29 22:17:37 +0530

    counts the total number of occurences of a given character in a String

--- Strings/LetterCount.java
+++ Strings/LetterCount.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class LetterCount {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter:");
+        String s=sc.nextLine();
+        System.out.println("Enter the character to check the repetition:");
+        char c=sc.next().charAt(0);
         int count=0;
-        String b="banana";
-        for(int i=0;i<b.length();i++){
-            if(b.charAt(i)=='n'){
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==c){
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("Repetiton count of "+c+" :"+count);
     }
 }
